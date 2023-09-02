c84a928d9d7055850df4cc62353f5a80083572b3
andrewDev08 <25936145-andrewDev08@users.noreply.replit.com>
2023-09-02 23:26:20 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,51 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string unitTestOneName = "Lucinda Potter";
+    string unitTestOneBirthdate = "6/24/1992";
+    string unitTestOneWork = "work 000-000-0101";
+    string unitTestOneCell = "cell 000-000-0189";
+    
+    Console.WriteLine(unitTestOneName); 
+    Console.WriteLine(unitTestOneBirthdate);
+    Console.WriteLine(unitTestOneWork);
+    Console.WriteLine(unitTestOneCell); 
+    Console.WriteLine(" ");
+    
+    string unitTestTwoName = "John Smith";
+    string unitTestTwoBirthdate = "8/15/1978";
+    string unitTestTwoWork = "work 000-111-0101";
+    string unitTestTwoCell = "cell 000-111-0189";
+    
+    Console.WriteLine(unitTestTwoName); 
+    Console.WriteLine(unitTestTwoBirthdate);
+    Console.WriteLine(unitTestTwoWork);
+    Console.WriteLine(unitTestTwoCell); 
+    Console.WriteLine(" ");
+
+    string unitTest3Name = "Chris Johnson";
+    string unitTest3Birthdate = "12/01/1978";
+    string unitTest3Work = "work 000-222-0101";
+    string unitTest3Cell = "cell 000-222-0189";
+    
+    Console.WriteLine(unitTest3Name); 
+    Console.WriteLine(unitTest3Birthdate);
+    Console.WriteLine(unitTest3Work);
+    Console.WriteLine(unitTest3Cell); 
+    Console.WriteLine(" ");
+
+    string unitTest4Name = "Robert Hall";
+    string unitTest4Birthdate = "2/27/1949";
+    string unitTest4Work = "work 000-444-0101";
+    string unitTest4Cell = "cell 000-444-0189";
+    
+    Console.WriteLine(unitTest4Name); 
+    Console.WriteLine(unitTest4Birthdate);
+    Console.WriteLine(unitTest4Work);
+    Console.WriteLine(unitTest4Cell); 
+    Console.WriteLine(" ");
+    
+  }
+
+}
